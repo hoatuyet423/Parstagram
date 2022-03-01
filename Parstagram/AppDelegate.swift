57e33700e29c4299c18f87c54f2c78a1d2e79ff0
Jenny Quach <65835856+hoatuyet423@users.noreply.github.com>
2022-03-01 04:54:36 -0800

    a clone of the instagram app, able to sign up, log in, post pictures and show posts on feed

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "XLGk9u4Nm6BlAeXGPs1Msb6ZvXY85LL90L0JpOs6" // <- UPDATE
+                $0.clientKey = "o561dIoZM7zcbnspPRnBV7UakpbOwi3pSufKZKrL" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
